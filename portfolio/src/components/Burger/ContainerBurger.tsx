692b435287a86a15241a3d07b1bd4323a396d22d
Agustina <agustinaayelensosa@gmail.com>
2024-08-06 08:36:31 -0300

    Estilos al navbar y Inicio del Home

--- portfolio/src/components/Burger/ContainerBurger.tsx
+++ portfolio/src/components/Burger/ContainerBurger.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 export const ContainerBurger: React.FC = () => {
   return (
     <div className='w-full h-[300px]'>
-        <button className='font-parrafo text-2xl text-sky-900'>Home</button>
-        <button className='font-parrafo text-2xl text-sky-900'>Sobre Mi</button>
-        <button className='font-parrafo text-2xl text-sky-900'>Proyectos</button>
+        <button className='font-parrafo text-2xl  text-[#77530a] hover:text-gray-600'>Home</button>
+        <button className='font-parrafo text-2xl  text-[#77530a] hover:text-gray-600'>Sobre Mi</button>
+        <button className='font-parrafo text-2xl  text-[#77530a] hover:text-gray-600'>Proyectos</button>
     </div>
   )
 }
--- portfolio/src/view/Home/Home.tsx
+++ portfolio/src/view/Home/Home.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import './Home.css'
 
 export const Home:React.FC = () => {
   return (
-    <div className='w-full min-h-screen'>
-
+    <div className='w-full min-h-screen bgHome flex justify-center items-center'>
+      <div className='w-4/5 h-[450px] estiloDorado flex justify-center items-center'>
+      <div className='w-2/3 h-[250px] bgSubCaja flex flex-col justify-center items-center'>
+        <h2 className='text-'>Bienvenidos </h2>
+          <h3>soy</h3>
+          <h1>Agustina Sosa</h1>
+          <h2>Desarrolladora web</h2>
+      </div>
+      </div>
     </div>
   )
 }
