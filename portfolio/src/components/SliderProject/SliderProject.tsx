27d5dc9a55156e6a16b41d0a96409bb33fdf9913
Agustina <agustinaayelensosa@gmail.com>
2024-10-14 07:10:58 -0300

    feat:vista de proyectos en curso

--- portfolio/src/components/SliderProject/SliderProject.tsx
+++ portfolio/src/components/SliderProject/SliderProject.tsx
@@ -1,55 +1,33 @@
-// import React from 'react'
-// import { Swiper, SwiperSlide } from "swiper/react";
+import { projects } from "../../helpers/arrayProject";
 
-// // Import Swiper styles
-// import "swiper/css";
-// import 'swiper/css/effect-coverflow';
-// import 'swiper/css/pagination';
-// import 'swiper/css/navigation';
+export const CardProject = () => {
+  return (
+    <div className="w-full h-full  flex  flex-wrap justify-center items-center gap-10 ">
+      {projects.map((project) => {
+        return (
+          <div
+            className="w-[40rem] h-60 flex  justify-center items-center bg-white border border-gray-200 rounded-lg shadow  hover:bg-gray-100 dark:border-gray-700 "
+            key={project.id}
+          >
+            <div className="w-2/3 h-full p-3">
+              <img
+                className="object-cover w-full h-full  "
+                src={project.image}
+                alt=""
+              />
+            </div>
 
-// import { EffectCoverflow, Pagination, Navigation } from "swiper";
-
-
-
-// export const SliderProject: React.FC = () => {
-//   return (
-//     <div>
-//       <h1>Proyectos</h1>
-//       <Swiper
-//       effect={'coverflow'}
-//       grabCursor={true}
-//       centeredSlides={true}
-//       loop={true}
-//       slidesPerView={'auto'}
-//       coverflowEffect={
-//         {
-//           rotate: 0,
-//           stretch: 0,
-//           depth: 100,
-//           modifier: 2.5
-//         }
-//       }
-//       className='swiper_container'
-//       >
-//         <SwiperSlide>
-//           <img src="" alt="" />
-//         </SwiperSlide>
-
-//         <SwiperSlide>
-//           <img src="" alt="" />
-//         </SwiperSlide>
-
-//         <SwiperSlide>
-//           <img src="" alt="" />
-//         </SwiperSlide>
-
-//         <div className='slider-controler'>
-//           <div className=''>
-
-//           </div>
-
-//         </div>
-//       </Swiper>
-//     </div>
-//   )
-// }
+            <div className="w-1/3 flex flex-col justify-evenly items-center">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {project.title}
+              </h5>
+              {/* <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                {project.description}
+              </p> */}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
---
+++ portfolio/src/helpers/arrayProject.ts
@@ -0,0 +1,41 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Ice Cream",
+    description: "",
+    image: "",
+    url: "",
+  },
+  {
+    id: 2,
+    title: "Stetic Glam",
+    description:
+      "Stetic Glam se dedica a ofrecer tratamientos avanzados de belleza y cuidado personal. Nuestra misión es realzar la belleza y confianza de nuestros clientes mediante servicios de alta calidad, adaptados a sus necesidades individuales.",
+    image: "https://i.ibb.co/SBwp4XY/steticglamhome.jpg",
+    url: "https://stetic-glam.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "RinDoor",
+    description:
+      "Aplicación que tenía como objetivo conectar clientes con profesionales. Los clientes realizaban publicaciones, y los profesionales debían subscribirse para poder postularse a esos trabajos. Valor agregado: 1. Incorporamos un chat cliente-profesional. Una vez que el cliente aceptaba a un profesional, se les habilitaba un chat para que puedan comunicarse más fácil. 2. Notificaciones vía email. 3. Ubicación del trabajo. En los detalles del trabajo incorporamos un mapa, (Google Maps), para que el profesional visualice la distancia de su casa hasta la ubicación del trabajo.",
+    image: "https://i.ibb.co/kxtG79n/Sin-rindoooooorrrrr.png",
+    url: "https://rindoor-2-0.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Eccommerce",
+    description:
+      "Experiencia académica: Henry Bootcamp. Descripción del proyecto: Aplicación que tenía como objetivo que un usuario invitado pueda registrarse, loguearse, ver un listado de productos, agregar uno o más productos al carrito, crear órdenes, etc. Frontend: Next, React, Typescript, Tailwind, Styled Component",
+    image: "https://i.ibb.co/rsBDgy1/proyecto4.png",
+    url: "https://github.com/agustina-sosa00/proyect4",
+  },
+  {
+    id: 5,
+    title: "Gestor de Turnos",
+    description:
+      "Experiencia académica: Henry Bootcamp. Descripción del proyecto: Aplicación que tenía como objetivo que un usuario invitado pueda registrarse, loguearse, poder guardar un turno y ver un listado de todos sus turnos. Frontend: React, Javascriipt, CSS Module,  Backend: NodeJS, Express, PostgreSQL, Typescript ",
+    image: "https://i.ibb.co/pP9Cqmw/proyecto3.png",
+    url: "https://github.com/agustina-sosa00/proyect3",
+  },
+];
