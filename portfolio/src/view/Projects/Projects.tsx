e8f5cfd3a405e83113bfc2750758aa34f1af3987
Agustina <agustinaayelensosa@gmail.com>
2024-10-17 16:34:47 -0300

    feat: termine la vista de proyectos y el footer

--- portfolio/src/view/Projects/Projects.tsx
+++ portfolio/src/view/Projects/Projects.tsx
@@ -5,9 +5,16 @@ export const Projects: React.FC = () => {
   return (
     <div
       id="/projects"
-      className="w-full min-h-screen  flex justify-center items-center pt-32 mb-10"
+      className="w-full min-h-screen flex flex-col justify-start items-center lg:pt-32 mb-10 gap-8 lg:gap-20"
     >
-      <CardProject />
+      <div className="w-full  flex justify-center items-center">
+        <h2 className="text-[#faa2b2] text-3xl md:text-6xl font-parrafo ">
+          Proyectos
+        </h2>
+      </div>
+      <div className="w-full min-h-screen  flex justify-center items-center ">
+        <CardProject />
+      </div>
     </div>
   );
 };
