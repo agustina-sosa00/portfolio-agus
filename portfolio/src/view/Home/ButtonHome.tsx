410e0a3c2a9cfc7dc0b986abeabb4c439b2d0564
Agustina <agustinaayelensosa@gmail.com>
2024-10-18 15:49:55 -0300

    refactor: correcciones en gral

--- portfolio/src/view/Home/ButtonHome.tsx
+++ portfolio/src/view/Home/ButtonHome.tsx
@@ -7,8 +7,8 @@ export const ButtonHome: React.FC = () => {
   return (
     <div className="w-full h-full flex justify-center items-center  z-10 ">
       <Link to="projects" smooth={true} duration={500}>
-        <button className="btn w-24 h-8 lg:w-24 lg:h-8 xl:w-36 xl:h-12 text-sm lg:text-base xl:text-xl rounded-xl outline-none text-white cursor-pointer hover:text-secundario  ">
-          Ver
+        <button className="btn w-24 h-8 lg:w-24 lg:h-8 xl:w-36 xl:h-12 text-sm lg:text-base xl:text-xl rounded-xl outline-none font-parrafo text-white cursor-pointer hover:text-secundario  ">
+          Proyectos
         </button>
       </Link>
     </div>
